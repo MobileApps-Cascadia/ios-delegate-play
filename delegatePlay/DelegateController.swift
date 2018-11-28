1965c0c70edbebff3e6c48ca6632771710b8afc9
Mei <meyrehay@gmail.com>
2018-11-27 17:00:36 -0800

    Finished the exercise

--- delegatePlay/DelegateController.swift
+++ delegatePlay/DelegateController.swift
@@ -8,19 +8,23 @@
 import UIKit
 
 // Accept the Protocol
-class DelegateController: UIViewController {
+class DelegateController: UIViewController, UseData {
 
     @IBOutlet weak var lblDelegate: UILabel!
     
     // Implement the protocol
-    func writeDatatoLabel(info: String) {
+    func writeDataToLabel(info: String) {
         //write the info into the label
+        lblDelegate.text = info
        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Assign this controller as the Delegate for the destination
-
+        let del = segue.destination
+        as! DelegatorController
+        
+        del.delegator = self
     }
 
 }
--- delegatePlay/DelegatorController.swift
+++ delegatePlay/DelegatorController.swift
@@ -10,15 +10,19 @@ import Foundation
 import UIKit
 
 //Create the protocol (also know as an interface) that the delegate will use
-
+protocol UseData{
+    func writeDataToLabel(info:String)
+}
 
 class DelegatorController:UIViewController {
 
     @IBOutlet weak var txtData: UITextField!
     //Add Delegate variable
+    var delegator:UseData?
     
     @IBAction func goButtonPressed() {
         //Call the Delegate's method
+        delegator!.writeDataToLabel(info:txtData.text!)
     }
 }
 
