b096f4fe4cbba59650797b38ad9d41838f5575bd
profile <juanitalaguilar@cc1-230-302.local>
2018-11-27 19:30:54 -0800

    changestofiles

--- delegatePlay/DelegatorController.swift
+++ delegatePlay/DelegatorController.swift
@@ -11,14 +11,20 @@ import UIKit
 
 //Create the protocol (also know as an interface) that the delegate will use
 
-
+protocol UseData {
+    func writeToLabel(info:String)
+    
+}
 class DelegatorController:UIViewController {
-
+   
     @IBOutlet weak var txtData: UITextField!
     //Add Delegate variable
+    var myDelegate:UseData?
     
     @IBAction func goButtonPressed() {
         //Call the Delegate's method
+        myDelegate!.writeToLabel(info:txtData.text!)
     }
+    
 }
 
