255b0ddfb176d1a3487bfd755bae5e02f2c7b690
VBeegle <VGBeegle@gmail.com>
2019-11-19 17:07:02 -0800

    Adds delegator to delegate functionality

--- delegatePlay/DelegateController.swift
+++ delegatePlay/DelegateController.swift
@@ -8,18 +8,20 @@
 import UIKit
 
 // Accept the Protocol
-class DelegateController: UIViewController {
+class DelegateController: UIViewController, UseData {
 
     @IBOutlet weak var descriptionLabel: UILabel!
     
     // Implement the protocol
-    func writeDatatoLabel(info: String) {
+    func writeToLabel(info: String) {
         //write the info into the label
-       
+       descriptionLabel.text = info
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Assign this controller as the Delegate for the destination
+        let delegator = segue.destination as! DelegatorController
+        delegator.myDelegate = self
 
     }
 
