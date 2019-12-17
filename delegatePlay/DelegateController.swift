cd0f4e1402cffd6d55c96d5f37a2011ad0f185d3
Stephen Gomez-Fox <gomezfox@uw.edu>
2019-12-16 19:39:54 -0800

    Code from slides

--- delegatePlay/DelegateController.swift
+++ delegatePlay/DelegateController.swift
@@ -8,19 +8,31 @@
 import UIKit
 
 // Accept the Protocol
-class DelegateController: UIViewController {
+class DelegateController: UIViewController, UseData {
+    func writeToLabel(info: String) {
+        <#code#>
+    }
+    
 
     @IBOutlet weak var descriptionLabel: UILabel!
     
     // Implement the protocol
     func writeDatatoLabel(info: String) {
         //write the info into the label
-       
+        UITextFieldDelegate.text = info;
+        
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         //Assign this controller as the Delegate for the destination
 
+        let delegator = segue.destination as! DelegateController
+        
+        delegator.myDelegate = self
+        
+        
     }
 
 }
